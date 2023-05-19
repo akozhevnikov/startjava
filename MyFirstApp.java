c91bd910c08e016960dead92058e84121229abd1
Anatoly K <kozhevnikov23@gmail.com>
2023-05-20 00:09:03 +0400

    finished test task 1 - 7

---
+++ MyFirstApp.java
@@ -0,0 +1,28 @@
+import java.lang.String;
+
+public class MyFirstApp {   
+    public static void main(String[] args) {
+        // System.out.println("Тест ");
+        // Scanner console = new Scanner(System.in);
+        // System.out.print("введите свое имя");
+        // System.out.println(console.nextLine());
+        byte num = 23;
+        int num_1 = 123123;
+
+        float num_2 = 23.45F;
+        double num_3 = 1231123.123123123D;
+
+        boolean isTrue = true;
+        char sym = 'R';
+
+        String str = "Hello World";
+
+        System.out.println(num);
+        System.out.println(num_1);
+        System.out.println(num_2);
+        System.out.println(num_3);
+        System.out.println(isTrue);
+        System.out.println(sym);
+        System.out.println(str);
+    }
+}
